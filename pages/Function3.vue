6e6ca8121de606870bcb35e228ed1309cbae254d
Mnagon <mnagon@hotmail.com>
2021-03-31 01:45:54 +0700

    Update function 3

--- pages/Function3.vue
+++ pages/Function3.vue
@@ -1,9 +1,43 @@
 <template>
-  <div class="container mx-auto max-w-screen-sm"></div>
+  <div class="container mx-auto max-w-screen-sm">
+    <p v-for="(arr, i) in testCase" :key="i">
+      {{ arr }} //{{ getSecondMax(arr) }}
+    </p>
+  </div>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      testCase: [
+        [],
+        [1],
+        [1, 2, 3],
+        [1, 1],
+        [1, 2, 3, 4, 5, 6],
+        [1, 5, 3, 2, 5, 10],
+        [100, 5, 3, 2, 99],
+        [35, 5, 3, 2, 5, 100],
+        [1, 5, 101, 2, 5, 10],
+        [10, 10, 9],
+      ],
+    }
+  },
+  computed: {},
+  methods: {
+    getSecondMax(arr: number[]): any {
+      const set = new Set(arr)
+      const uniqueArr = [...set]
+      if (uniqueArr.length < 1) {
+        return null
+      } else if (uniqueArr.length === 1) {
+        return arr[0]
+      } else {
+        return uniqueArr.sort((a, b) => b - a)[1]
+      }
+    },
+  },
+})
 </script>
-
-<style></style>
